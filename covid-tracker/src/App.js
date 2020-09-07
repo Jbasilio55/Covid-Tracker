792a7473f74c056839ffedbed279dc325ad86f0d
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-09-07 14:02:14 -0400

    4th commit folder structered

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-class App extends React.Component{
-    render(){
+class App extends React.Component {
+    render() {
         return (
             <div>
                 <h1>App</h1>
